4013016ab045371be94b3a2bc64817bbfe3ada23
Subilan <suyuanliang1995@gmail.com>
2023-10-14 09:39:30 +0800

    load

--- src/main/java/red/oases/nuker/Nuker.java
+++ src/main/java/red/oases/nuker/Nuker.java
@@ -1,7 +1,10 @@
 package red.oases.nuker;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import red.oases.nuker.Commands.Executor;
+import red.oases.nuker.Commands.Tab;
 
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public final class Nuker extends JavaPlugin {
@@ -13,6 +16,8 @@ public final class Nuker extends JavaPlugin {
         Files.load(this);
         logger = getLogger();
         getServer().getPluginManager().registerEvents(new EffectLayerListener(), this);
+        Objects.requireNonNull(getCommand("nuker")).setExecutor(new Executor());
+        Objects.requireNonNull(getCommand("nuker")).setTabCompleter(new Tab());
     }
 
     @Override
